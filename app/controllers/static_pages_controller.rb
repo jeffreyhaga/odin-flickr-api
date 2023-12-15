2c54b801d01a99dc5ea0457b7319e0318867509e
Jeff Haga <jeffreyrhaga@gmail.com>
2023-12-15 06:45:15 -0500

    Hiding keys and finishing seach

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,19 @@
 class StaticPagesController < ApplicationController
 
-  require 'flickraw'
+def index
+  # Use Rails credentials to securely fetch your API key and secret
+  flickr_api_key = Rails.application.credentials.flickr[:api_key]
+  flickr_shared_secret = Rails.application.credentials.flickr[:secret_key]
 
-  def index
-  end
+  # Initialize your Flickr client with the credentials
+  flickr = Flickr.new(flickr_api_key, flickr_shared_secret)
+
+  user_id = params[:user_id]
+
+  @photos = flickr.people.getPhotos(user_id: user_id)
+
+end
+
+
+
 end

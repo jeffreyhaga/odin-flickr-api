6961905bf427a6e3051355d952d54f456393cf96
Jeff Haga <jeffreyrhaga@gmail.com>
2023-12-16 10:26:06 -0500

    Added Stimulus Framework features

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = [ "output", "hide", "show", "color" ]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.showTarget.hidden = true
   }
+
+  hide() {
+    this.outputTarget.hidden = true
+    this.hideTarget.hidden = true
+    this.showTarget.hidden = false
+  }
+
+  show() {
+    this.outputTarget.hidden = false
+    this.hideTarget.hidden = false
+    this.showTarget.hidden = true
+  }
+
+  color() {
+    this.outputTarget.style.backgroundColor = this.element.querySelector('#color').checked ? "yellow" : ""; 
+  }
+
 }
